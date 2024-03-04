e360125f656f2933ca7c935517ea6c3b0341ce71
emersooc <emersooc@mail.uc.edu>
2024-03-04 14:42:53 -0500

    Part B and C Finished/Start Part D

--- src/SafeInput.java
+++ src/SafeInput.java
@@ -4,7 +4,7 @@ public class SafeInput
 {
     public static String getNonZeroLenString(Scanner pipe, String prompt)
     {
-        String retString = ""; //sets length to zero
+        String retString = ""; //sets to empty string
 
         do
         {
@@ -17,6 +17,46 @@ public class SafeInput
     }
 
     public static int getInt(Scanner pipe, String prompt)
+    {
+        boolean done = false; //boolean to control while loop
+        int retInt = 0; //sets to zero
+
+        do
+        {
+            System.out.print("\n" +prompt+ ":"); //prompt here
+
+            if (pipe.hasNextInt())
+            {
+                retInt = pipe.nextInt();
+                done = true;
+            }
+        }
+        while(!done);
+
+        return retInt;
+    }
+
+    public static double getDouble(Scanner pipe, String prompt)
+    {
+        boolean done = false; //boolean to control while loop
+        int retDouble = 0; //sets to zero
+
+        do
+        {
+            System.out.print("\n" +prompt+ ":"); //prompt here
+
+            if (pipe.hasNextDouble())
+            {
+                retDouble = pipe.nextInt();
+                done = true;
+            }
+        }
+        while(!done);
+
+        return retDouble;
+    }
+
+    public static int getRangedInt(Scanner pipe, String prompt)
     {
 
     }
