0a408c2b182ad43d281bc8fedb7850b526e3a7ae
emersooc <emersooc@mail.uc.edu>
2024-03-04 14:24:44 -0500

    Part A Finished/Start Part B

--- src/SafeInput.java
+++ src/SafeInput.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class SafeInput
 {
-    public static void main(String[] args)
+    public static String getNonZeroLenString(Scanner pipe, String prompt)
+    {
+        String retString = ""; //sets length to zero
+
+        do
+        {
+            System.out.print("\n" +prompt+ ":"); //prompt will output here
+            retString = pipe.nextLine();
+        }
+        while(retString.length() == 0);
+
+        return retString;
+    }
+
+    public static int getInt(Scanner pipe, String prompt)
     {
 
     }
